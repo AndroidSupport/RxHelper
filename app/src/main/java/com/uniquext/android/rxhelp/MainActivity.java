82c19a7c3ee5284641a3956d1dbc8c1e126b5ec1
penghaitao <penghaitao918@163.com>
2019-05-06 18:59:19 +0800

    Demo

--- app/src/main/java/com/uniquext/android/rxhelp/MainActivity.java
+++ app/src/main/java/com/uniquext/android/rxhelp/MainActivity.java
@@ -1,13 +1,47 @@
 package com.uniquext.android.rxhelp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.uniquext.android.rxlifecycle.base.RxAppCompatActivity;
+import com.uniquext.android.rxlifecycle.event.ActivityEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends RxAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Observable.interval(1000L, TimeUnit.MILLISECONDS)
+                .compose(bindUntilEvent(ActivityEvent.STOP))
+                .subscribe(new Observer<Long>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+                        Log.i("####", "onNext " + aLong);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.i("####", "onComplete ");
+                    }
+                });
     }
 }
